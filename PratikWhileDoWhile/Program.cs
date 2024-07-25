dfb2474d4479b95180b63ac025a62dd52d9edf23
pc <pc@DESKTOP-78M62IA>
2024-07-25 14:08:36 +0300

    Add project files.

---
+++ PratikWhileDoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿//While ile yapılmış hali
+Console.WriteLine("Lütfen bir değer giriniz: ");
+int limit = Convert.ToInt32(Console.ReadLine());
+int sayac = 0;
+
+Console.WriteLine();
+Console.WriteLine("While");
+
+while (sayac <= limit - 1)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}
+
+//Do-While ile yapılmış hali
+Console.WriteLine();
+Console.WriteLine("Do-While");
+
+sayac = 0;
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+} while (sayac <= limit - 1);
